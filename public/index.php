4c0a408d4b43c197424e2206788aa02f4282b1da
lizhili <lizhilimaster@163.com>
2020-04-05 09:47:43 +0800

    修改thinkphp问题，拦截中文控制器

--- public/index.php
+++ public/index.php
@@ -16,6 +16,13 @@ namespace think;
 require __DIR__ . '/../thinkphp/base.php';
 
 // 支持事先使用静态方法设置Request对象和Config对象
-
+//解决thinkphp 本地的bug 拦截控制器不能为 汉字和空格 特殊字符
+$path=\think\facade\Request::path();
+if($path and !preg_match('/^[A-Za-z0-9][\w|\.]*$/', $path)){
+    //重定向浏览器
+    header("Location:/404.html");
+    //确保重定向后，后续代码不会被执行
+    exit;
+}
 // 执行应用并响应
 Container::get('app')->bind('index')->run()->send();
